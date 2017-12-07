fd0d0338bf8203824fd2a2a7368bcb33f6a64f34
warburtonn <lsnakemanl@gmail.com>
2017-12-07 13:36:14 +0200

    Add files via upload
    LoginForm.form - visual create LoginForm
    LoginForm.java - method swing create Login window and use java data base connectivity

---
+++ LoginForm.java
@@ -0,0 +1,64 @@
+package com.diplom;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+/**
+ * Created by Сергей on 13.09.2017.
+ */
+    public class LoginForm {
+    private JTextField passField;
+    private JButton loginButton;
+    private JPanel LoginFrame;
+    private JTextField nameField;
+    private JLabel jLabelLogin;
+    private static final String NAME="root";
+    private static final String PASSWORD = "root";
+    private static final String URL = "jdbc:mysql://localhost:3306/diplom";
+
+    //public mainWindow()
+        {
+
+        }
+    public LoginForm() {
+
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(null,"Login press..");
+                int flag = 1;
+
+                try {
+                    Connection connection = DriverManager.getConnection(URL, NAME, PASSWORD);
+                    Statement statementLogin = connection.createStatement();
+                    ResultSet resultLogin = statementLogin.executeQuery("SELECT * FROM diplom.userdatabase");
+                    while (resultLogin.next()) {
+                        if (resultLogin.getString(2).equals(nameField.getText()) && resultLogin.getString(3).equals(passField.getText()))
+
+                            flag = 0;
+                        //break;
+
+
+                        if (flag == 0) {
+                            jLabelLogin.setText("Login succesful");
+                        } else
+                            jLabelLogin.setText("Invalid Name or password");
+                    }
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame panel = new JFrame("Авторизация");
+        panel.setSize(300, 180);
+        panel.setContentPane(new LoginForm().LoginFrame);
+        panel.setLocationRelativeTo(null);
+        panel.setVisible(true);
+        panel.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+}
